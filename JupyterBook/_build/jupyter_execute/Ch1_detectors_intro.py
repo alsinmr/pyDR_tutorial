fd9676cf6a63474d05572ca4a3c54352bb28fbe5
Andy <Albert.Smith-Penzel@medizin.uni-leipzig.de>
2023-08-21 14:13:22 +0200

    Moved index file

--- JupyterBook/_build/jupyter_execute/Ch1_detectors_intro.py
+++ JupyterBook/_build/jupyter_execute/Ch1_detectors_intro.py
@@ -25,48 +25,77 @@
 # In this simple case, the correlation time, $\tau_c$, indicates how quickly the correlation decays. For motion, a shorter $\tau_c$ indicates a faster motion, and a longer $\tau_c$ indicates a slower motion. $\langle X(\tau)\rangle_\tau$ is simply the average of the parameter $X(\tau)$, and ($\langle X(t)^2\rangle_\tau-\langle X(t)\rangle_\tau^2$) is its variance (standard deviation squared)
 # 
 # It can be, however, that multiple motions affect the decorrelation, in which case the correlation function can be modeled more generally as
+# 
 # $$
 # \begin{equation}
 # C(t)=\langle X(t)\rangle_\tau^2+\sum\limits_i{A_i\exp(-t/\tau_i)}
 # \end{equation}
 # $$
+# 
 # In this case, the $A_i$ add up to the total variance ($\langle X(t)^2\rangle_\tau-\langle X(t)\rangle_\tau^2$), but each corresponds to a different correlation time, representing contributions from different motions with varying speeds. This expression suggests a number of discrete motions, but it is also possible to obtain a continuum of correlation times, in which case we would better represent the situation with an infinite number of amplitudes, given as an integral:
+# 
+# $$
 # \begin{equation}
 # C(t)=\langle X(t)\rangle_\tau^2+\int\limits_{-\infty}^\infty{\theta(z)\exp(-t/(10^z\cdot1\mathrm{ s})dz}
 # \end{equation}
+# $$
+# 
 # In this representation, $z$ is the log-correlation time ($z=\log_{10}(\tau_c/\mathrm{s})$), and $\theta(z)$ is the distribution of correlation times as a function of the log-correlation time. $\theta(z)$ must then integrate to the total variance.
 
 # ## The angular correlation function for NMR
 # In NMR, a major source of relaxation is due to the stochastic reoriention of anisotropy interaction tensors, such as dipole couplings, chemical shift anisotropies (CSA), and quadrupole couplings. Then, we cannot use a strictly linear correlation function, but rather must use the rank-2 tensor correlation function (which can be constructed from a sum of linear correlation functions).
+# 
+# $$
 # \begin{equation}
 # C(t)=\left\langle\frac{3\cos^2\beta_{\tau,t+\tau}-1}{2}\right\rangle_\tau
 # \end{equation}
+# $$
+# 
 # Here, $\beta_{\tau,t+\tau}$ is the angle between the z-component of the relevant tensor at some time $\tau$ and a later time, $t+\tau$. For example, for a one-bond dipole coupling, this is just the angle between the bond between the two times $\tau$ and $t+\tau$.
 # 
 # Given the full correlation function, a given relaxation rate constant may be obtained by first calculating the spectral density function, $J(\omega)$, which is the Fourier transform of the correlation function
+# 
+# $$
 # \begin{equation}
 # J(\omega)=\frac25\int\limits_0^\infty{C(t)\exp(-i\omega t)dt}.
 # \end{equation}
+# $$
+# 
 # Then, for example, $T_1$ relaxation of a $^{13}$C nucleus due to a single bonded $^1$H and its CSA is given by
+# 
+# $$
 # \begin{equation}
 # R_1=\left(\frac{\delta_\mathrm{HC}}{4}\right)^2(J(\omega_\mathrm{H}-\omega_\mathrm{C})+3J(\omega_\mathrm{C})+6J(\omega_\mathrm{H}+\omega_\mathrm{C}))+\frac13(\omega_\mathrm{C}\Delta\sigma)^2J(\omega_\mathrm{C})
 # \end{equation}
+# $$
+# 
 # In this equation, $R_1$ is the longitudinal relaxation rate constant for $^{13}$C ($1/T_1=R_1$), with $\delta_\mathrm{HC}$ being the anisotropy of the dipole coupling and $\Delta\sigma_\mathrm{C}$ the width of the CSA in ppm. The $J(\omega)$ are sampled at the $^1$H and $^13$C Larmor frequencies in addition to their sums and differences (given in radians/s).
 # 
 # Usually, relaxation due to reorientational motion can be expressed as a sum over the spectral density sampled at various frequencies:
+# 
+# $$
 # \begin{equation}
 # R_\zeta=\sum\limits_i{a_iJ(\omega_i)}
 # \end{equation}.
+# $$
 # 
 # The models of the correlation function given above are still valid, but we typically write them as:
+# 
+# $$
 # \begin{eqnarray}
 # C(t)&=&S^2+(1-S^2)\sum\limits_i{A_i\exp(-t/\tau_i}) \\
 # &=&S^2+(1-S^2)\int\limits_{-\infty}^\infty{\theta(z)\exp(-t/(10^z\cdot\mathrm{ s}))dz}
 # \end{eqnarray}
+# $$
+# 
 # Here, $S^2$ is defined as:
+# 
+# $$
 # \begin{equation}
 # S^2=\lim_{t\rightarrow\infty}C(t)
 # \end{equation}
+# $$
+# 
 
 # ## Fitting dynamics data
 # Whether we have NMR data, or other dynamics data depending on some form of time-correlation function, we can, in principle, fit the experimental data based on some model of the correlation function. However, the challenge is, if we don't know the model, how valuable are these fits? We'll start by taking some example motion, characterized by 3 motions and calculate a few relaxation rate constants for those motions.
@@ -145,9 +174,13 @@ def fun(x):
 
 
 # First, we just try to fit with the original model, i.e. with a tri-exponential decay
+# 
+# $$
 # \begin{equation}
 # C(t)=1-(A_0+A_1+A_2)+A_0\exp(-t/\tau_0)+A_1\exp(-t/\tau_1)+A_2\exp(-t/\tau_2)
 # \end{equation}
+# $$
+# 
 
 # In[5]:
 
@@ -233,32 +266,55 @@ ax.figure.tight_layout()
 
 # ## Basic detector theory
 # Suppose that our correlation function is given by an arbitrarily complex sum of decaying exponential terms. We'll take this to be the rank-2 tensor correlation function used in NMR (here, that just means we assume an initial value of one, and value at $t=\infty$ of $S^2$). Then, this can be written as a distribution of correlation times
+# 
+# $$
 # \begin{equation}
 # C(t)=S^2+(1-S^2)\int\limits_{-\infty}^\infty{\theta(z)\exp(-t/(10^z\cdot\mathrm{1 s}))dz}
 # \end{equation}
+# $$
+# 
 # The distribution, $(1-S^2)\theta(z)$, can be assumed to be a sum of $\delta$-functions, allowing it to also be re-written as a sum of correlation times:
+# 
+# $$
 # \begin{equation}
 # C(t)=S^2+(1-S^2)\sum\limits_i{A_i\exp(-t/\tau_i)}
 # \end{equation}
+# $$
 # 
 # Then, a given relaxation rate constant is given by
+# 
+# $$
 # \begin{equation}
 # R_\zeta^{(\theta,S)}=\sum\limits_i{a_\zeta^iJ^{(\theta,S)}(\omega_i)}
 # \end{equation}
+# $$
+# 
 # The spectral density, $J^{(\theta,S)}(\omega)$ is, indeed, a function of the distribution of correlation times, given by
+# 
+# $$
 # \begin{equation}
 # J^{(\theta,S)}(\omega)=2(1-S^2)\int\limits_{-\infty}^\infty{\theta(z)\frac{10^z\cdot\mathrm{1 s}}{1+(\omega\cdot10^z\cdot\mathrm{1 s})^2}dz}
 # \end{equation}
+# $$
+# 
 # Then, a given relaxation rate constant can be written as a function of the distribution of correlation times:
+# 
+# $$
 # \begin{eqnarray}
 # R_\zeta^{(\theta,S)}&=&\sum\limits_i{a_\zeta^iJ^{(\theta,S)}(\omega_i)} \\
 # &=&\sum\limits_i{a_\zeta^i2(1-S^2)\int\limits_{-\infty}^\infty{\theta(z)\frac{10^z\cdot\mathrm{1 s}}{1+(\omega_i\cdot10^z\cdot\mathrm{1 s})^2}}dz}\\
 # &=&(1-S^2)\int\limits_{-\infty}^\infty{2\sum\limits_i{a_\zeta^i\frac{10^z\cdot\mathrm{1 s}}{1+(\omega_i\cdot10^z\cdot\mathrm{1 s})^2}}dz}\\
-# &=&(1-S^2)\int\limits_{-\infty}^\infty{\theta(z)R_\zeta(z)dz}\end{eqnarray}
+# &=&(1-S^2)\int\limits_{-\infty}^\infty{\theta(z)R_\zeta(z)dz}
+# \end{eqnarray}
+# $$
+# 
 # where
+# 
+# $$
 # \begin{equation}
 # R_\zeta(z)=2\sum\limits_i{a_\zeta^i\frac{10^z\cdot\mathrm{1 s}}{1+(\omega_i\cdot10^z\cdot\mathrm{1 s})^2}}
 # \end{equation}
+# $$
 # 
 # While the math may seem a bit overwhelming at first, all we've really done is a little shuffling of terms. We point out that a given relaxation rate constant is sensitive to certain correlation times more than others, and its measurement basically provides us a 'window', given by $R_\zeta(z)$, into the total dynamics. We have termed this window a *detector sensitivity*, which looks at only a certain range of correlation times, as defined by the integral above. pyDR automatically calculates these windows, as shown below, for a $^{15}$N $T_1$ (protein backbone) acquired at 600 MHz.
 
@@ -271,16 +327,23 @@ _=pyDR.Sens.NMR(Type='R1',Nuc='15N',v0=600).plot_Rz()
 # As one sees, measurement of this $T_1$ lets us characterize dynamics *around* roughly 1 ns. 
 # 
 # Interestingly, if we have multiple relaxation rate constants, we may add them together to get new windows. For example, we define:
+# 
+# $$
 # \begin{eqnarray}
 # \rho_n^{(\theta,S)}&=&\sum\limits_\zeta{b_\zeta^n R_\zeta^{(\theta,S)}} \\
 # &=&\sum\limits_\zeta{b_\zeta^n(1-S^2)\int\limits_{-\infty}^\infty{\theta(z)R_\zeta(z)dz}} \\
 # &=&(1-S^2)\int\limits_{-\infty}^\infty{\theta(z)\sum\limits_\zeta{b_\zeta^n R_\zeta(z)}dz}\\
 # &=&(1-S^2)\int\limits_{-\infty}^\infty{\theta(z)\rho_n(z)dz}
 # \end{eqnarray}
+# $$
+# 
 # with
+# 
+# $$
 # \begin{equation}
 # \rho_n(z)=\sum\limits_\zeta{b_\zeta^n R_\zeta(z)}
 # \end{equation}
+# $$
 # 
 # The superscript $n$ just indicates that there are multiple possible linear combinations of relaxation rate constants. Each linear combination creates a new window, but most possible windows may not be particularly useful. For example, we take two relaxation rate constants below, and show a random set of linear combinations:
 
