e89dad2d414838a4a44cef37a49b64e48691286c
Andy <Albert.Smith-Penzel@medizin.uni-leipzig.de>
2023-09-22 09:33:39 +0200

    Added blank template

---
+++ JupyterBook/_build/jupyter_execute/Tpl_blank.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # <font color="maroon">Template: pyDR Setup</font>
+
+# <a href="https://githubtocolab.com/alsinmr/pyDR_tutorial/blob/main/ColabNotebooks/Tpl_blank.ipynb" target="_blank"><img src="https://colab.research.google.com/assets/colab-badge.svg"></a>
+
+# This template only downloads and imports pyDR. Intended for use on Google Colab.
+
+# In[1]:
+
+
+# SETUP pyDR
+import os
+os.chdir('../..')
+
+
+# In[2]:
+
+
+#Imports
+import pyDR
+
+
+# ## Your code below:
+
+# In[ ]:
+
+
+
+
